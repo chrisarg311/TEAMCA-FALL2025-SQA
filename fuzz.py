b66cc30cc8004e853196b654ce2fb48a2ee8bc4b
chrisarg311 <chrisarg311@gmail.com>
2025-12-08 22:33:18 -0600

    Change import statements to dynamic loading
    Refactor imports to use dynamic loading for git.repo.miner.

--- fuzz.py
+++ fuzz.py
@@ -1,8 +1,14 @@
 import unittest
+import importlib.util
 from pathlib import Path
 
-from MLForensics-farzana/mining/git.repo.miner.py import getPythonCount, getMLStats
-from MLForensics-farzana/mining/mining.py import giveTimeStamp, makeChunks, days_between
+module_path = Path("MLForensics-farzana/mining/git.repo.miner.py")
+spec = importlib.util.spec_from_file_location("git.repo.miner", module_path)
+gitrepominer = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(gitrepominer)
+
+#from MLForensics_farzana/mining/git.repo.miner.py import getPythonCount, getMLStats
+#from MLForensics_farzana/mining/mining.py import giveTimeStamp, makeChunks, days_between
 
 
 class getPythonCountTests(unittest.TestCase):
