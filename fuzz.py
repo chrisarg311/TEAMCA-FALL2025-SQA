4f48a914b3e718c73a179d615ede584f1570d5cf
chrisarg311 <chrisarg311@gmail.com>
2025-12-08 22:24:16 -0600

    Add tests

--- fuzz.py
+++ fuzz.py
@@ -1,1 +1,11 @@
+import unittest
+from pathlib import Path
 
+from MLForensics-farzana/mining/git.repo.miner.py import getPythonCount, getMLStats
+from MLForensics-farzana/mining/mining.py import giveTimeStamp, makeChunks, days_between
+
+
+class getPythonCountTests(unittest.TestCase):
+
+  def getPythonCount1(self):
+    self.assertEqual(getPythonCount(Path(__file__).resolve().parentprint(script_dir)), 1)
